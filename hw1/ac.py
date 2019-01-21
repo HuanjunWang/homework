0810f4e39c042977f696eaccb185521653d44455
HuanjunWang <153147512@qq.com>
2019-01-21 14:38:45 +0800

    ac fail

--- hw1/ac.py
+++ hw1/ac.py
@@ -1,27 +1,25 @@
-import pickle
 import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
-from keras.optimizers import Adam
+from keras.optimizers import Adam,SGD
 import gym
-
 import tensorflow as tf
 
-ENVS = 'Humanoid-v2'
+ENVS = 'Ant-v2'
 SIGMA = 5.
+LEARNING_RATE = 1e-5
 
-
-def verify_policy(policy_fun):
+def verify_policy(env, policy_fun):
     class ARGS(object):
         pass
 
     args = ARGS()
     args.envname = ENVS
-    args.max_timesteps = 1000
-    args.num_rollouts = 10
-    args.render = False
+    args.max_timesteps = 300
+    args.num_rollouts = 1
+    args.render = True
 
-    env = gym.make(args.envname)
+    #env = gym.make(args.envname)
     max_steps = args.max_timesteps or env.spec.timestep_limit
 
     returns = []
@@ -49,11 +47,15 @@ def verify_policy(policy_fun):
 
 def make_gauss_model(input_shape, action_num):
     model = Sequential()
-    model.add(Dense(units=128, activation='relu', input_shape=input_shape))
+    model.add(Dense(units=256, activation='relu', input_shape=input_shape))
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=256, activation='relu'))
     model.add(Dense(units=256, activation='relu'))
     model.add(Dense(units=action_num, activation='linear'))
 
-    adam = Adam(lr=1e-2)
+    adam = Adam(lr=LEARNING_RATE)
+    sgd = SGD(lr=LEARNING_RATE)
 
     def maximum_likehood(y_true, mu):
         normal_d = tf.distributions.Normal(loc=mu, scale=SIGMA)
@@ -65,66 +67,90 @@ def make_gauss_model(input_shape, action_num):
 
 def make_value_model(input_shape):
     model = Sequential()
-    model.add(Dense(units=128, activation='relu', input_shape=input_shape))
+    model.add(Dense(units=256, activation='relu', input_shape=input_shape))
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=256, activation='relu'))
     model.add(Dense(units=256, activation='relu'))
     model.add(Dense(units=1, activation='linear'))
 
-    adam = Adam(lr=1e-3)
+    adam = Adam(lr=LEARNING_RATE)
+    sgd = SGD(lr=LEARNING_RATE)
     model.compile(optimizer=adam, loss='mse')
     return model
+
+
+def step1_sample(env, policy_fun, min_steps=500):
+    states = []
+    actions = []
+    rewards = []
+    next_states = []
+    final_state = []
+
+    steps = 0
+    while steps < min_steps:
+        s = env.reset()
+        done = False
+        while not done:
+            action = policy_fun(s[None, :])
+            n_s, r, done, _ = env.step(action)
+            steps += 1
+
+            states.append(s)
+            actions.append(action)
+            rewards.append(r)
+            next_states.append(n_s)
+            if done:
+                final_state.append(0)
+            else:
+                final_state.append(1)
+
+            s = n_s
+
+    return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(final_state)
 
 
 def ac():
     env = gym.make(ENVS)
     input_shape = env.observation_space.shape
-    # input_shape= (input_shape[0])
     action_num = env.action_space.shape[0]
+
+    print("Input Shape:", input_shape)
+    print("Action Number:", action_num)
+
     policy_model = make_gauss_model(input_shape, action_num)
     value_model = make_value_model(input_shape)
 
     def policy_fun_gauss(obs):
         mu = policy_model.predict(obs)
-        return np.random.normal(mu, SIGMA)
+        return np.random.normal(mu, SIGMA)[0]
 
-    for epoch in range(1000):
+    for epoch in range(10000):
         print("epoch:", epoch)
+
         # Step 1, Sample
-        observations = []
-        actions = []
-        rewards_to_go = []
-        action_with_advance = []
-
-        for i in range(100):
-            obs = env.reset()
-            done = False
-            totalr = 0.
-            steps = 0
-            rewards = []
-            while not done:
-                action = policy_fun_gauss(obs[None, :])
-                observations.append(obs)
-                actions.append(action)
-                obs, r, done, _ = env.step(action)
-                rewards.append(r)
-                totalr += r
-                steps += 1
-
-            for j in range(len(rewards)):
-                rewards_to_go.append(sum(rewards[j:]))
+        current_states, actions, rewards, next_states, final_state = step1_sample(env, policy_fun_gauss, min_steps=5000)
 
         # Step 2, Fit the value model
-        value_model.fit(np.array(observations), np.array(rewards_to_go), batch_size=128, epochs=10, verbose=0)
+        for _ in range(10):
+            next_value = value_model.predict(np.array(next_states))
+            target_value = np.array(rewards) + next_value.ravel() * final_state * .99
+            value_model.fit(np.array(current_states), target_value, batch_size=128, epochs=1, verbose=0)
 
         # Step 3, improve the policy model
-        for (obs, action, reward) in zip(observations, actions, rewards_to_go):
-            baseline = value_model.predict(obs[np.newaxis])[0, 0]
-            advance = reward - baseline
-            action_with_advance.append(np.append(action, advance))
+        next_v = value_model.predict(next_states).ravel()
+        this_v = value_model.predict(current_states).ravel()
+        advances = rewards + next_v * final_state - this_v
+        print("Advances:", np.mean(advances))
 
-        policy_model.fit(np.array(observations), np.array(action_with_advance), batch_size=128, epochs=1, verbose=2)
+        policy_model.fit(current_states, np.hstack((actions, advances[:, None])), batch_size=128, epochs=1, verbose=2)
+
+        def policy_fun_gauss2(obs):
+            mu = policy_model.predict(obs)
+            return np.random.normal(mu, .01)[0]
 
         if epoch % 10 == 0:
-            verify_policy(policy_fun_gauss)
+            verify_policy(env, policy_fun_gauss2)
 
 
 if __name__ == '__main__':
